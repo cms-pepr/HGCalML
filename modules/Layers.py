new options for coordinate masking and fix in transform layer for gradient

modules/Layers.py
@@ -71,9 +71,44 @@ class CreateZeroMask(Layer):
 
 global_layers_list['CreateZeroMask']=CreateZeroMask
 
+
+
+
+
+class AveragePoolVertices(Layer):
+    def __init__(self, keepdims=False, **kwargs):
+        super(AveragePoolVertices, self).__init__(**kwargs)
+        self.keepdims=keepdims
+    
+    def compute_output_shape(self, input_shape):
+        if self.keepdims:
+            return input_shape
+        return (input_shape[0],input_shape[2])
+    
+    def call(self, inputs):
+        #create mask
+        n_nonmasked = tf.cast(tf.count_nonzero(tf.reduce_sum(inputs, axis=2), axis=-1), dtype='float32')
+        n_nonmasked = tf.expand_dims(n_nonmasked, axis=1)
+        rsum = tf.reduce_sum(inputs, axis=1)
+        out = rsum/(n_nonmasked+K.epsilon())
+        if self.keepdims:
+            out = tf.expand_dims(out, axis=1)
+            out = tf.tile(out, [1, tf.shape(inputs)[1], 1])
+        return out
+    
+    def get_config(self):
+        config = {'keepdims': self.keepdims}
+        base_config = super(AveragePoolVertices, self).get_config()
+        return dict(list(base_config.items()) + list(config.items() ))
+    
+        
+    
+
+global_layers_list['CreateZeroMask']=CreateZeroMask
+
    
 class TransformCoordinates(Layer):
-    def __init__(self, feature_index_x, feature_index_y, feature_index_z, **kwargs):
+    def __init__(self, feature_index_x=0, feature_index_y=1, feature_index_z=2, **kwargs):
         super(TransformCoordinates, self).__init__(**kwargs)
         self.feature_index_x=feature_index_x
         self.feature_index_y=feature_index_y
@@ -89,11 +124,12 @@ class TransformCoordinates(Layer):
         zcoord = inputs[:,:,self.feature_index_z:self.feature_index_z+1]
         	
         #transform to spherical coordinates
-        r = tf.math.sqrt( xcoord**2 + ycoord**2 + zcoord**2 )
-        theta = tf.math.acos( zcoord / r )
-        phi = tf.math.atan( ycoord / xcoord )
+        r = tf.math.sqrt( xcoord**2 + ycoord**2 + zcoord**2 +K.epsilon())
+        theta = tf.math.acos( zcoord / (r+K.epsilon()) )
+        phi = tf.math.atan( ycoord / (xcoord+K.epsilon()) )
         
-        ##replace nan values with 0 to deal with divergences		
+        ##replace nan values with 0 to deal with divergences
+        #-> does not work for the gradient, needs to be fied before		
         thetazeros = tf.zeros(shape=tf.shape(theta))
         phizeros = tf.zeros(shape=tf.shape(phi))    
         theta = tf.where(tf.is_nan(theta), thetazeros, theta)

modules/caloGraphNN_keras.py
@@ -25,7 +25,11 @@ class GlobalExchange(keras.layers.Layer):
 
 class GravNet(keras.layers.Layer):
     def __init__(self, n_neighbours, n_dimensions, n_filters, n_propagate, name, 
-                 also_coordinates=False, feature_dropout=-1, **kwargs):
+                 also_coordinates=False, feature_dropout=-1, 
+                 coordinate_kernel_initializer=keras.initializers.Orthogonal(),
+                 fix_coordinate_space=False, 
+                 masked_coordinate_offset=None,
+                 **kwargs):
         super(GravNet, self).__init__(**kwargs)
 
         self.n_neighbours = n_neighbours
@@ -35,16 +39,26 @@ class GravNet(keras.layers.Layer):
         self.name = name
         self.also_coordinates = also_coordinates
         self.feature_dropout = feature_dropout
+        self.masked_coordinate_offset = masked_coordinate_offset
         
         self.input_feature_transform = keras.layers.Dense(n_propagate, name = name+'_FLR')
-        self.input_spatial_transform = keras.layers.Dense(n_dimensions, name = name+'_S')
+        self.input_spatial_transform = keras.layers.Dense(n_dimensions, name = name+'_S', kernel_initializer=coordinate_kernel_initializer)
         self.output_feature_transform = keras.layers.Dense(n_filters, activation='tanh', name = name+'_Fout')
 
         self._sublayers = [self.input_feature_transform, self.input_spatial_transform, self.output_feature_transform]
+        if fix_coordinate_space:
+            self.input_spatial_transform = None
+            self._sublayers = [self.input_feature_transform, self.output_feature_transform]
+        
+        
 
     def build(self, input_shape):
+        if self.masked_coordinate_offset is not None:
+            input_shape = input_shape[0]
+            
         self.input_feature_transform.build(input_shape)
-        self.input_spatial_transform.build(input_shape)
+        if self.input_spatial_transform is not None:
+            self.input_spatial_transform.build(input_shape)
         
         # tf.ragged FIXME?
         self.output_feature_transform.build((input_shape[0], input_shape[1], input_shape[2] + self.input_feature_transform.units * 2))
@@ -56,11 +70,27 @@ class GravNet(keras.layers.Layer):
         super(GravNet, self).build(input_shape)
 
     def call(self, x):
+        
+        if self.masked_coordinate_offset is not None:
+            if not isinstance(x, list):
+                raise Exception('GravNet: in mask mode, input must be list of input,mask')
+            mask = x[1]
+            x = x[0]
+            print('mask',mask.shape)
+            mask = tf.tile(mask, [1,1,tf.shape(x)[2]])
+            
         features = self.input_feature_transform(x)
+        
         if self.feature_dropout>0 and self.feature_dropout < 1:
             features = keras.layers.Dropout(self.feature_dropout)(features)
         
-        coordinates = self.input_spatial_transform(x)
+        if self.input_spatial_transform is not None:
+            coordinates = self.input_spatial_transform(x)
+        else:
+            coordinates = x[:,:,0:self.n_dimensions]
+            
+        if self.masked_coordinate_offset is not None:
+            coordinates = tf.where(mask>0., coordinates, tf.zeros_like(coordinates)-self.masked_coordinate_offset)
 
         collected_neighbours = self.collect_neighbours(coordinates, features)
 
@@ -71,6 +101,8 @@ class GravNet(keras.layers.Layer):
         return self.output_feature_transform(updated_features)
 
     def compute_output_shape(self, input_shape):
+        if self.masked_coordinate_offset is not None:
+            input_shape = input_shape[0]
         if self.also_coordinates:
             return [(input_shape[0], input_shape[1], self.output_feature_transform.units),
                     (input_shape[0], input_shape[1], self.n_dimensions)]
@@ -125,7 +157,8 @@ class GravNet(keras.layers.Layer):
                       'n_propagate': self.n_propagate,
                       'name':self.name,
                       'also_coordinates': self.also_coordinates,
-                      'feature_dropout' : self.feature_dropout}
+                      'feature_dropout' : self.feature_dropout,
+                      'masked_coordinate_offset'       : self.masked_coordinate_offset}
             base_config = super(GravNet, self).get_config()
             return dict(list(base_config.items()) + list(config.items()))
 