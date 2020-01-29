e260df83b686d1f85a30d12bba718b202b0e5544
Jan Kieseler <jkiesele@cern.ch>
2020-01-29 18:14:30 +0100

    fix for compiled libs

--- env.sh
+++ env.sh
@@ -5,6 +5,6 @@ export DEEPJETCORE_SUBPACKAGE=$HGCALML
 
 export PATH=$HGCALML/scripts:$PATH
 export PYTHONPATH=$HGCALML/modules:$PYTHONPATH
+export LD_LIBRARY_PATH=$HGCALML/modules:$LD_LIBRARY_PATH
 #?export PYTHONPATH=$HGCALML/modules/datastructures:$PYTHONPATH
 #for ffmpeg
-export PATH=$PATH:/afs/cern.ch/work/j/jkiesele/public/conda_env/miniconda3/envs/djcenv/bin/
