a3fca73ae4a7018aef64dc32c29139787671b7a6
Jan Kieseler <jkiesele@cern.ch>
2020-01-29 14:43:42 +0100

    changed env

--- env.sh
+++ env.sh
@@ -1,18 +1,10 @@
 
-#! /bin/bash
 
-
-eval "$(/afs/cern.ch/work/j/jkiesele/public/conda_env/miniconda3/bin/conda shell.bash hook)"
-
-THISDIR=`pwd`
-cd /afs/cern.ch/work/j/jkiesele/public/conda_env/DeepJetCore
-source env.sh
-cd $THISDIR
 export HGCALML=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd -P)
 export DEEPJETCORE_SUBPACKAGE=$HGCALML
 
-
-cd $HGCALML
 export PATH=$HGCALML/scripts:$PATH
 export PYTHONPATH=$HGCALML/modules:$PYTHONPATH
-export PYTHONPATH=$HGCALML/modules/datastructures:$PYTHONPATH
+#?export PYTHONPATH=$HGCALML/modules/datastructures:$PYTHONPATH
+#for ffmpeg
+export PATH=$PATH:/afs/cern.ch/work/j/jkiesele/public/conda_env/miniconda3/envs/djcenv/bin/
