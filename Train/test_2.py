f3ecb5520fd7fcad3dc10aaf8c5f59318b40d758
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2020-04-21 15:56:59 +0200

    feat: Add check after 100 batches

--- Train/test_2.py
+++ Train/test_2.py
@@ -10,62 +10,63 @@ from DeepJetCore.modeltools import DJCKerasModel
 from DeepJetCore.training.training_base import training_base
 from tensorflow.keras import Model
 
+# from tensorflow.keras.models import load_model
+from DeepJetCore.training.training_base import custom_objects_list
+
+# from tensorflow.keras.optimizer_v2 import Adam
+
 from ragged_callbacks import plotEventDuringTraining
 from DeepJetCore.DJCLayers import ScalarMultiply
 
-#tf.compat.v1.disable_eager_execution()
+
+# tf.compat.v1.disable_eager_execution()
 
 
 
 def ser_simple_model(Inputs):
-    
-    
     x = Dense(128)(Inputs[0])
-    
-    beta    = Dense(1, activation='sigmoid') (x) 
-    energy  = Dense(1, activation=None)      (x) 
-    eta     = Dense(1, activation=None)      (x) 
-    phi     = Dense(1, activation=None)      (x) 
-    ccoords = Dense(2, activation=None)      (x) 
+
+    beta = Dense(1, activation='sigmoid')(x)
+    energy = Dense(1, activation=None)(x)
+    eta = Dense(1, activation=None)(x)
+    phi = Dense(1, activation=None)(x)
+    ccoords = Dense(2, activation=None)(x)
 
     x = Concatenate()([
-        beta    ,
-        energy  ,
-        eta     ,
-        phi     ,
-        ccoords ])
-        
-    return Model(inputs=Inputs, outputs=[x,x])
+        beta,
+        energy,
+        eta,
+        phi,
+        ccoords])
+
+    return Model(inputs=Inputs, outputs=[x, x])
 
 
 def gravnet_model(Inputs, feature_dropout=-1.):
-    nregressions=5
+    nregressions = 5
 
-    
     # I_data = tf.keras.Input(shape=(num_features,), dtype="float32")
     # I_splits = tf.keras.Input(shape=(1,), dtype="int32")
 
     I_data = Inputs[0]
     I_splits = tf.cast(Inputs[1], tf.int32)
 
-    
-
     x_data, x_row_splits = RaggedConstructTensor()([I_data, I_splits])
-  
-    input_features = x_data #these are going to be passed through for the loss
-    
+
+    input_features = x_data  # these are going to be passed through for the loss
+
     x_basic = BatchNormalization(momentum=0.6)(x_data)  # mask_and_norm is just batch norm now
     x = x_basic
 
     n_filters = 0
     n_gravnet_layers = 5
-    feat=[x_basic]
+    feat = [x_basic]
     for i in range(n_gravnet_layers):
         n_filters = 128
         n_propagate = 64
-        n_neighbours=200
-        if i%2:
-            n_neighbours=40
+        n_neighbours = 200
+        if i % 2:
+            n_neighbours = 40
 
         x = RaggedGlobalExchange()([x, x_row_splits])
         x = Dense(64, activation='elu')(x)
@@ -73,10 +74,10 @@ def gravnet_model(Inputs, feature_dropout=-1.):
         x = Dense(64, activation='elu')(x)
         x = BatchNormalization(momentum=0.6)(x)
         x = RaggedGravNet_simple(n_neighbours=n_neighbours,
-                           n_dimensions=4,
-                           n_filters=n_filters,
-                           n_propagate=n_propagate,
-                           name='gravnet_' + str(i))([x, x_row_splits])
+                                 n_dimensions=4,
+                                 n_filters=n_filters,
+                                 n_propagate=n_propagate,
+                                 name='gravnet_' + str(i))([x, x_row_splits])
         x = BatchNormalization(momentum=0.6)(x)
         feat.append(Dense(48, activation='elu')(x))
 
@@ -84,16 +85,16 @@ def gravnet_model(Inputs, feature_dropout=-1.):
     x = Dense(128, activation='elu')(x)
     x = Dense(64, activation='elu')(x)
     x = Dense(64, activation='elu')(x)
-    
+
     beta = Dense(1, activation='sigmoid')(x)
-    energy = ScalarMultiply(100.) (Dense(1, activation=None)(x))
+    energy = ScalarMultiply(100.)(Dense(1, activation=None)(x))
     eta = Dense(1, activation=None)(x)
     phi = Dense(1, activation=None)(x)
     ccoords = Dense(2, activation=None)(x)
-    
-    print('input_features',input_features.shape)
 
-    x = Concatenate()([input_features, beta,energy,eta,phi,ccoords])
+    print('input_features', input_features.shape)
+
+    x = Concatenate()([input_features, beta, energy, eta, phi, ccoords])
 
     # x = Concatenate(name="concatlast", axis=-1)([x,coords])#+[n_showers]+[etas_phis])
     predictions = x
@@ -102,16 +103,17 @@ def gravnet_model(Inputs, feature_dropout=-1.):
 
     return Model(inputs=Inputs, outputs=[predictions, predictions])
 
-    
 
-train=training_base(testrun=False,resumeSilently=True,renewtokens=False)
-
+train = training_base(testrun=False, resumeSilently=True, renewtokens=False)
 
 from Losses import obj_cond_loss_truth, obj_cond_loss_rowsplits, null_loss
 
-#train.setDJCKerasModel(simple_model)
-train.setModel(gravnet_model)#ser_simple_model)
-#train.keras_model.dynamic=True
+# optimizer = Adam(lr=1e-4)
+# train.setCustomOptimizer(optimizer)
+
+# train.setDJCKerasModel(simple_model)
+train.setModel(gravnet_model)  # ser_simple_model)
+# train.keras_model.dynamic=True
 train.compileModel(learningrate=1e-4,
                    loss=[obj_cond_loss_truth, obj_cond_loss_rowsplits])
 ####### do not use metrics here - keras problem in TF 2.2rc0
@@ -119,59 +121,58 @@ train.compileModel(learningrate=1e-4,
 
 print(train.keras_model.summary())
 
+nbatch = 15000  # **2 #this will be an upper limit on vertices per batch
 
-nbatch=15000#**2 #this will be an upper limit on vertices per batch
-
-verbosity=2
+verbosity = 2
 import os
 
 samplepath = train.val_data.getSamplePath(train.val_data.samples[0])
-callbacks=[]
-for i in range(4):
-    plotoutdir=train.outputDir+"/event_"+str(i+2)
-    os.system('mkdir -p '+plotoutdir)
+callbacks = []
+for i in range(10):
+    plotoutdir = train.outputDir + "/event_" + str(i + 2)
+    os.system('mkdir -p ' + plotoutdir)
     callbacks.append(
         plotEventDuringTraining(
-            outputfile=plotoutdir+"/sn",
+            outputfile=plotoutdir + "/sn",
             samplefile=samplepath,
-            after_n_batches=100,
+            after_n_batches=300,
             batchsize=100000,
             on_epoch_end=False,
-            use_event=2+i)
-        )
-#train.saveModel("testmodel.h5")
-
-#train.loadModel("TESTTF115_4/testmodel.h5")
-
-#exit()
-
-model,history = train.trainModel(nepochs=10, 
-                                 run_eagerly=True,
-                                 batchsize=nbatch,
-                                 batchsize_use_sum_of_squares=False,
-                                 checkperiod=1, # saves a checkpoint model every N epochs
-                                 verbose=verbosity,
-                                 additional_callbacks=callbacks)
+            use_event=2 + i)
+    )
 
 
 
+
+print("It should save now")
+model, history = train.trainModel(nepochs=10,
+                                  run_eagerly=True,
+                                  batchsize=nbatch,
+                                  batchsize_use_sum_of_squares=False,
+                                  checkperiod=1,  # saves a checkpoint model every N epochs
+                                  verbose=verbosity,
+                                  backup_after_batches=100,
+                                  additional_callbacks=callbacks)
 
 exit()
 
 train.change_learning_rate(2e-4)
 
-model,history = train.trainModel(nepochs=5+1, 
-                                 batchsize=nbatch,
-                                 run_eagerly=True,
-                                 batchsize_use_sum_of_squares=False,
-                                 checkperiod=1, # saves a checkpoint model every N epochs
-                                 verbose=verbosity,)
+model, history = train.trainModel(nepochs=5 + 1,
+                                  batchsize=nbatch,
+                                  run_eagerly=True,
+                                  batchsize_use_sum_of_squares=False,
+                                  checkperiod=1,  # saves a checkpoint model every N epochs
+                                  backup_after_batches=100,
+                                  verbose=verbosity, )
 
 train.change_learning_rate(1e-4)
-model,history = train.trainModel(nepochs=99+5+1, 
-                                 batchsize=nbatch,
-                                 run_eagerly=True,
-                                 batchsize_use_sum_of_squares=False,
-                                 checkperiod=1, # saves a checkpoint model every N epochs
-                                 verbose=verbosity,)
+model, history = train.trainModel(nepochs=99 + 5 + 1,
+                                  batchsize=nbatch,
+                                  run_eagerly=True,
+                                  batchsize_use_sum_of_squares=False,
+                                  checkperiod=1,  # saves a checkpoint model every N epochs
+                                  backup_after_batches=100,
+                                  verbose=verbosity, )
+
 
