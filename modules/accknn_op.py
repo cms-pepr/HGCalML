66bb69fc5f8edc06064c0802cd21f0f63e11dabf
Jan Kieseler <jkiesele@cern.ch>
2020-06-18 15:29:24 +0200

    removing obsolete kernels

--- modules/accknn_op.py
+++ modules/accknn_op.py
@@ -53,41 +53,5 @@ def _AccumulateKnnGrad(op, grad, gradmaxidxs):
   
   
 
-_accknn_nd_op = tf.load_op_library('accumulate_knn_nd.so')
-_accknn_nd_grad_op = tf.load_op_library('accumulate_knn_nd_grad.so')
-
-def AccumulateKnnNd(coords,  features, indices, n_moments=0):
-    if n_moments > 3 or n_moments<0:
-        raise ValueError("AccumulateKnnNd: n_moments must be between 0 and 3 (including)")
-    return _accknn_nd_op.AccumulateKnnNd(n_moments=n_moments, coords=coords,  features=features, indices=indices)
-
-
-@ops.RegisterGradient("AccumulateKnnNd")
-def _AccumulateKnnNdGrad(op, grad, gradmaxidxs, gradfeatsum):
-  """
-    
-  """
-  
-  n_coords = op.inputs[0].shape[1]
-  n_features = op.inputs[1].shape[1]
-  n_neigh = op.inputs[2].shape[1]
-  
-  coords  = op.inputs[0]
-  features  = op.inputs[1]
-  max_feat_indices = op.outputs[1]
-  orig_op_out_feat = op.outputs[0]
-  feat_sum = op.outputs[2]
-  neigh_indices = op.inputs[2]
-  
-  coord_grad , feat_grad = _accknn_nd_grad_op.AccumulateKnnNdGrad(grad_from_out_features=grad,
-                                                                  grad_from_sum_features=gradfeatsum,
-                                                             coords=coords,
-                                                             features=features,
-                                                             neigh_indices=neigh_indices,
-                                                             max_feat_indices=max_feat_indices,
-                                                             orig_op_out_feat=orig_op_out_feat,
-                                                             orig_up_out_feat_sum = feat_sum)
-
-  return [coord_grad , feat_grad, None] #no gradient for indices
   
   
