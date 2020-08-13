ee9806fb4df350495bbde0b657adbe3a9299a495
Jan Kieseler <jkiesele@cern.ch>
2020-08-13 15:01:15 +0200

    added xyt output

--- Train/default_training_double_pool.py
+++ Train/default_training_double_pool.py
@@ -135,8 +135,8 @@ def gravnet_model(Inputs, feature_dropout=-1.,momentum=0.6):
     beta = Add()([all_beta_p,beta])
     
     
-    eta = Dense(1, activation=None, name="dense_eta", kernel_initializer='zeros')(x)
-    phi = Dense(1, activation=None, name="dense_phi", kernel_initializer='zeros')(x)
+    xy = Dense(2, activation=None, name="dense_xy", kernel_initializer='zeros')(x)
+    t = ScalarMultiply(1e-9)(Dense(1, activation=None, name="dense_t", kernel_initializer='zeros')(x))
     ccoords = Dense(2, activation=None, name="dense_ccoords")(x)
     
     ccoords = Add()([ScalarMultiply(0.5)(ccoords), ScalarMultiply(0.25)(c_a), ScalarMultiply(0.25)(c_b)])
@@ -146,7 +146,7 @@ def gravnet_model(Inputs, feature_dropout=-1.,momentum=0.6):
 
     print('input_features', input_features.shape)
 
-    x = Concatenate(name="concat_final")([input_features, beta, energy, eta, phi, ccoords])
+    x = Concatenate(name="concat_final")([input_features, beta, energy, xy,t, ccoords])
 
     # x = Concatenate(name="concatlast", axis=-1)([x,coords])#+[n_showers]+[etas_phis])
     predictions = x
--- Train/default_training_parse_clr.py
+++ Train/default_training_parse_clr.py
@@ -71,15 +71,15 @@ def gravnet_model(Inputs, feature_dropout=-1.):
     x = Dense(64, activation='elu',name="dense_last_c")(x)
 
     beta = Dense(1, activation='sigmoid', name="dense_beta")(x)
-    eta = Dense(1, activation=None, name="dense_eta",kernel_initializer='zeros')(x)
-    phi = Dense(1, activation=None, name="dense_phi",kernel_initializer='zeros')(x)
+    xy = Dense(2, activation=None, name="dense_xy",kernel_initializer='zeros')(x)
+    t = ScalarMultiply(1e-9)(Dense(1, activation=None, name="dense_t",kernel_initializer='zeros')(x))
     ccoords = Dense(2, activation=None, name="dense_ccoords")(x)
     energy = Dense(1, activation=None,name="dense_en_final")(x)
     energy = ExpMinusOne(name="en_scaling")(energy)
 
     print('input_features', input_features.shape)
 
-    x = Concatenate(name="concat_final")([input_features, beta, energy, eta, phi, ccoords])
+    x = Concatenate(name="concat_final")([input_features, beta, energy, xy, t, ccoords])
 
     # x = Concatenate(name="concatlast", axis=-1)([x,coords])#+[n_showers]+[etas_phis])
     predictions = x
