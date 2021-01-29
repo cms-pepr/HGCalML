1d0098cc2c41f5c589951e1455e782a799411e4b
Kenneth Long <kdlong@hep.wisc.edu>
2021-01-29 16:28:26 +0100

    I think it's working

--- modules/datastructures/TrainData_NanoML.py
+++ modules/datastructures/TrainData_NanoML.py
@@ -2,12 +2,8 @@ from DeepJetCore.TrainData import TrainData, fileTimeOut
 from DeepJetCore.compiled.c_simpleArray import simpleArray
 import uproot3 as uproot
 import awkward0 as ak
-from numba import jit
-import ROOT
-import os
 import pickle
 import gzip
-import tensorflow as tf
 import numpy as np
 
     
@@ -22,13 +18,13 @@ class TrainData_NanoML(TrainData):
         # off = np.cumsum(ak.to_numpy(ak.num(jagged)))
         # off = np.insert(off, 0, 0)
         jagged = ak.JaggedArray.concatenate([x for x in obs], axis=1)
-        off = jagged.offsets
+        off = np.array(jagged.offsets, dtype='int64')
         if flatten:
-            jagged = jagged.flatten()
+            jagged = np.array(jagged.flatten(), dtype='float32')
         return (jagged, off) if offsets else jagged
 
     def truthObjects(self, sc, indices, null):
-        return np.where(indices.flatten() < 0, null, sc[indices].flatten())
+        return np.array(np.where(indices.flatten() < 0, null, sc[indices].flatten()), dtype='float32')
         
     def convertFromSourceFile(self, filename, weighterobjects, istraining, treename="Events"):
         return self.base_convertFromSourceFile(filename, weighterobjects, istraining, treename=treename)
@@ -41,7 +37,7 @@ class TrainData_NanoML(TrainData):
 
         hits = ["RecHitHGC"+x for x in ["EE", "HEF", "HEB"]]
         recHitEnergy, offsets = self.hitObservable(tree, hits, "energy", offsets=True)
-        recHitSimClusIdx = self.hitObservable(tree, hits, "MergedSimClusterIdx", flatten=False)
+        recHitSimClusIdx = self.hitObservable(tree, hits, "SimClusterIdx", flatten=False)
         recHitX = self.hitObservable(tree, hits, "x")
         recHitY = self.hitObservable(tree, hits, "y")
         recHitZ = self.hitObservable(tree, hits, "z")
@@ -51,25 +47,23 @@ class TrainData_NanoML(TrainData):
         recHitTheta = np.arccos(recHitZ/recHitR)
         recHitEta = -np.log(np.tan(recHitTheta/2))
 
-        simClusterEnergy = tree["MergedSimCluster_boundaryEnergy"].array()
-        simClusterX = tree["MergedSimCluster_impactPoint_x"].array()
-        simClusterY = tree["MergedSimCluster_impactPoint_y"].array()
-        simClusterZ = tree["MergedSimCluster_impactPoint_z"].array()
-        simClusterTime = tree["MergedSimCluster_impactPoint_t"].array()
-        simClusterPdgId = tree["MergedSimCluster_pdgId"].array()
+        simClusterEnergy = tree["SimCluster_boundaryEnergy"].array()
+        simClusterX = tree["SimCluster_impactPoint_x"].array()
+        simClusterY = tree["SimCluster_impactPoint_y"].array()
+        simClusterZ = tree["SimCluster_impactPoint_z"].array()
+        simClusterTime = tree["SimCluster_impactPoint_t"].array()
+        simClusterPdgId = tree["SimCluster_pdgId"].array()
 
-        print(simClusterPdgId, len(simClusterPdgId))
-        print(recHitSimClusIdx, len(recHitSimClusIdx))
-        recHitTruthPID = self.truthObjects(simClusterPdgId, recHitSimClusIdx, 0)
+        recHitTruthPID = self.truthObjects(simClusterPdgId, recHitSimClusIdx, 0.)
         recHitTruthEnergy = self.truthObjects(simClusterEnergy, recHitSimClusIdx, -1)
         recHetTruthEnergyMu = np.where(np.abs(recHitTruthPID) == 13, recHitEnergy, recHitTruthEnergy)
-        recHitTruthX = self.truthObjects(simClusterX, recHitSimClusIdx, 0)
-        recHitTruthY = self.truthObjects(simClusterY, recHitSimClusIdx, 0)
-        recHitTruthZ = self.truthObjects(simClusterZ, recHitSimClusIdx, 0)
+        recHitTruthX = self.truthObjects(simClusterX, recHitSimClusIdx, 1.)
+        recHitTruthY = self.truthObjects(simClusterY, recHitSimClusIdx, 0.)
+        recHitTruthZ = self.truthObjects(simClusterZ, recHitSimClusIdx, 0.)
         recHitTruthTime = self.truthObjects(simClusterZ, recHitSimClusIdx, -1)
         recHitTruthR = np.sqrt(recHitTruthX*recHitTruthX+recHitTruthY*recHitTruthY+recHitTruthZ*recHitTruthZ)
-        recHitTruthTheta = np.arccos(recHitTruthZ/recHitTruthR)
-        recHitTruthPhi = np.arctan(recHitTruthY/recHitTruthX)
+        recHitTruthTheta = np.arccos(np.divide(recHitTruthZ, recHitTruthR, out=np.zeros_like(recHitTruthZ), where=recHitTruthR!=0))
+        recHitTruthPhi = np.arctan(np.divide(recHitTruthY, recHitTruthX, out=np.zeros_like(recHitTruthY), where=recHitTruthX!=0))
         recHitTruthEta = -np.log(np.tan(recHitTruthTheta/2))
 
         features = np.array(np.concatenate([
@@ -85,27 +79,28 @@ class TrainData_NanoML(TrainData):
             ], axis=-1), dtype='float32')
 
         farr = simpleArray()
-        farr.createFromNumpy(features, np.array(offsets, dtype='int64'))
+        farr.createFromNumpy(features, offsets)
         del features  
 
+        recHitSimClusIdx = np.array(recHitSimClusIdx.flatten(), dtype='float32')
         truth = np.concatenate([
-            recHitSimClusIdx.flatten(), # 0
+            recHitSimClusIdx, # 0
             recHitTruthEnergy,
             recHitTruthX,
             recHitTruthY,
             recHitTruthZ,  #4
-            #truthHitAssignedDirX,
-            #truthHitAssignedDirY, #6
-            #truthHitAssignedDirZ,
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDirX,
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDirY, #6
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDirZ,
             recHitTruthEta     ,
             recHitTruthPhi,
             recHitTruthTime,  #10
-            #truthHitAssignedDirEta,
-            #truthHitAssignedDirR,
-            #truthHitAssignedDepEnergies, #16
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDirEta,
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDirR,
+            np.zeros(len(recHitEnergy), dtype='float32'), #truthHitAssignedDepEnergies, #16
             
-            ticlHitAssignementIdx  , #17
-            ticlHitAssignedEnergies, #18
+            np.zeros(len(recHitEnergy), dtype='float32'), #ticlHitAssignementIdx  , #17
+            np.zeros(len(recHitEnergy), dtype='float32'), #ticlHitAssignedEnergies, #18
             recHitTruthPID #19 - 19+n_classes #won't be used anymore
             
             ], axis=-1)
@@ -113,23 +108,23 @@ class TrainData_NanoML(TrainData):
         
         
         t_idxarr = simpleArray()
-        t_idxarr.createFromNumpy(truthHitAssignementIdx, rs)
+        t_idxarr.createFromNumpy(recHitSimClusIdx, offsets)
         
         t_energyarr = simpleArray()
-        t_energyarr.createFromNumpy(truthHitAssignedEnergies,rs)
+        t_energyarr.createFromNumpy(recHitTruthEnergy, offsets)
         
         t_posarr = simpleArray()
-        t_posarr.createFromNumpy(np.concatenate([truthHitAssignedX, truthHitAssignedY],axis=-1),rs)
+        t_posarr.createFromNumpy(np.concatenate([recHitTruthX, recHitTruthY],axis=-1), offsets)
         
         t_time = simpleArray()
-        t_time.createFromNumpy(truthHitAssignedT,rs)
+        t_time.createFromNumpy(recHitTruthTime, offsets)
         
         t_pid = simpleArray()
-        t_pid.createFromNumpy(truthHitAssignedPIDs,rs)
+        t_pid.createFromNumpy(recHitTruthPID, offsets)
         
         #remaining truth is mostly for consistency in the plotting tools
         t_rest = simpleArray()
-        t_rest.createFromNumpy(truth,rs)
+        t_rest.createFromNumpy(truth, offsets)
         
         return [farr, t_idxarr, t_energyarr, t_posarr, t_time, t_pid],[t_rest], []
 
