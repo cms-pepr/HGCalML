1129375ea54edf0d81356ac751a1eff7c363eb46
Jan Kieseler <jkiesele@cern.ch>
2021-02-09 09:08:26 +0100

    fixes for triton compilation

--- modules/lossLayers.py
+++ modules/lossLayers.py
@@ -163,7 +163,7 @@ class LLLocalClusterCoordinates(LossLayerBase):
         
         #distances are actuallt distances**2
         expdist = tf.exp(- 3. * distances)
-        att_proto = (1.-repulsion_contrib)* distances # ( (1.-expdist) + 0.01*distances ) #mild attractive to help learn
+        att_proto = (1.-repulsion_contrib)* ( (1.-expdist) + 0.01*distances ) #mild attractive to help learn
         rep_proto = repulsion_contrib * (expdist - 0.001*distances)
         
         #hierarchy = tf.clip_by_value(hierarchy,0.,1-1e-6)
