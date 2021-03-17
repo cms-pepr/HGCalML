e08cf6813f87ced375a8397fa6e4fcf390e82643
Jan Kieseler <jkiesele@cern.ch>
2021-03-17 17:11:54 +0100

    covariance calculation - no gradient but super fast

---
+++ modules/neighbour_covariance_op.py
@@ -0,0 +1,34 @@
+
+import tensorflow as tf
+from tensorflow.python.framework import ops
+
+
+
+_ncov = tf.load_op_library('neighbour_covariance.so')
+
+def NeighbourCovariance(coordinates, features, n_idxs):
+    '''
+    .Input("coordinates: float32")
+    .Input("features: float32")
+    .Input("n_idxs: int32")
+    .Output("covariance: float32")
+    .Output("means float32");
+
+    '''
+    
+    covariance, means = _ncov.NeighbourCovariance(coordinates=coordinates, 
+                                                  features=features, 
+                                                  n_idxs=n_idxs)
+    return covariance, means
+
+
+@ops.RegisterGradient("NeighbourCovariance")
+def _NeighbourCovarianceGrad(op, covariancegrad, meansgrad):
+    
+    return None, None, None #no grad for row splits and masking values
+    
+    
+    
+    
+    
+    
