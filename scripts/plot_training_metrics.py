74dcc41a106b623b32bf0b45edbb301ba97d6b8a
Shah Rukh Qasim <ishahrukhqasim@gmail.com>
2021-08-19 15:19:11 +0200

    Update plot_training_metrics.py

--- scripts/plot_training_metrics.py
+++ scripts/plot_training_metrics.py
@@ -1,31 +1,44 @@
-#!/usr/bin/env python3
-
 import sql_credentials
 import experiment_database_reading_manager
 import matplotlib.pyplot as plt
 import numpy as np
+import argparse
+from matplotlib.backends.backend_pdf import PdfPages
 
 def running_mean(x, w):
     return np.convolve(x, np.ones(w), 'same') / w
 
 
+parser = argparse.ArgumentParser(
+    'Produce running metrics plot (loss, efficiency and more)')
+parser.add_argument('experiment_name',
+                    help='Experiment name')
+parser.add_argument('output',
+                    help='PDF file')
+
+
+args = parser.parse_args()
+
+
+
 manager = experiment_database_reading_manager.ExperimentDatabaseReadingManager(mysql_credentials=sql_credentials.credentials)
 print("Getting data now")
-training_performance_metrics = manager.get_data('training_performance_metrics_extended', experiment_name='alpha_experiment_june_pca_double_cords_240e008f')
+# args.exper
+training_performance_metrics = manager.get_data('training_performance_metrics_extended', experiment_name=args.experiment_name)
 print("Received data")
 
 
 average_over = 10
 
+pdf = PdfPages(args.output)
 training_performance_metrics['efficiency'] = [float(x) for x in training_performance_metrics['efficiency']]
-
 plt.figure()
 eff = training_performance_metrics['efficiency']
 eff = running_mean(eff, average_over)
 plt.plot(training_performance_metrics['iteration'], eff)
 plt.xlabel('iteration')
 plt.ylabel('efficiency')
-
+pdf.savefig()
 
 
 training_performance_metrics['fake_rate'] = [float(x) for x in training_performance_metrics['fake_rate']]
@@ -36,7 +49,7 @@ fak = running_mean(fak, average_over)
 plt.plot(training_performance_metrics['iteration'], fak)
 plt.xlabel('iteration')
 plt.ylabel('fake_rate')
-
+pdf.savefig()
 
 training_performance_metrics['sum_response'] = [float(x) for x in training_performance_metrics['sum_response']]
 
@@ -46,6 +59,7 @@ sres = running_mean(sres, average_over)
 plt.plot(training_performance_metrics['iteration'], sres)
 plt.xlabel('iteration')
 plt.ylabel('response')
+pdf.savefig()
 
 
 
@@ -56,7 +70,6 @@ plt.plot(training_performance_metrics['iteration'], loss)
 plt.xlabel('iteration')
 plt.ylabel('loss')
 
+pdf.savefig()
 
-plt.show()
-
-
+pdf.close()
