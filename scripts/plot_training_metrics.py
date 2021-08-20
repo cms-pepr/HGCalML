6bd64ad17f71e15a7d93995c5f770e89a60d417b
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2021-08-20 11:23:55 +0200

    feat: Add support for file, and running average and also add plots for number of predicted showers and number of truth showers

--- scripts/plot_training_metrics.py
+++ scripts/plot_training_metrics.py
@@ -13,12 +13,13 @@ def running_mean(x, w):
 
 parser = argparse.ArgumentParser(
     'Produce running metrics plot (loss, efficiency and more)')
-parser.add_argument('-experiment_name',
-                    help='Experiment name',default='')
-parser.add_argument('-database_file',
-                    help='Database output file with training metrics (normally train_path/training_metrics.db',default='')
-parser.add_argument('-plot_all',
-                    help='Plot only a few metrics for faster performance or all of them?',default='False')
+parser.add_argument('experiment_name',
+                    help='Experiment name on the server (normally exists in model_train_output/unique_id.txt')
+parser.add_argument('--is_database_file', help='If you want to plot from a database file instead of server \nThe database file is normally located in model_train_output/training_metrics.db', action='store_true')
+parser.add_argument('--plot_all',
+                    help='Plot only a few metrics for faster performance or all of them?',action='store_true')
+parser.add_argument('--running_average',
+                    help='N running average elements, 1 means no running average', default='10')
 parser.add_argument('output',
                     help='PDF file')
 
@@ -26,26 +27,33 @@ parser.add_argument('output',
 args = parser.parse_args()
 
 
-if len(args.experiment_name) != 0:
+if not args.is_database_file:
     print("Gonna get data from the server, using experiment_name %s" % args.experiment_name)
     manager = experiment_database_reading_manager.ExperimentDatabaseReadingManager(mysql_credentials=sql_credentials.credentials)
     training_performance_metrics = manager.get_data('training_performance_metrics_extended', experiment_name=args.experiment_name)
 else:
-    manager = experiment_database_reading_manager.ExperimentDatabaseReadingManager(file=args.database_file)
+    manager = experiment_database_reading_manager.ExperimentDatabaseReadingManager(file=args.experiment_name)
     training_performance_metrics = manager.get_data('training_performance_metrics_extended')
 
 
 
-average_over = 10
+average_over = int(args.running_average)
+
+if average_over<=0:
+    print("Error in running average, should be 1 or more (and less than number of iterations)")
 
 
 def plot_metric(metric):
-    global training_performance_metrics
+    global training_performance_metrics, average_over
     training_performance_metrics[metric] = [float(x) for x in training_performance_metrics[metric]]
     plt.figure()
-    eff = training_performance_metrics[metric]
-    eff = running_mean(eff, average_over)
-    plt.plot(training_performance_metrics['iteration'], eff)
+    y_values = training_performance_metrics[metric]
+    if average_over > len(y_values):
+        print("Running over can't be greater than number of iterations...")
+        raise RuntimeError("Running over can't be greater than number of iterations...")
+    if average_over > 1:
+        y_values = running_mean(y_values, average_over)
+    plt.plot(training_performance_metrics['iteration'], y_values)
     plt.xlabel('iteration')
     plt.ylabel(metric)
     pdf.savefig()
