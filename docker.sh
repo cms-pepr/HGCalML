b3c65337625722e2dac782d05599fd91fdc66d2a
Philipp Zehetner <philipp.zehetner@cern.ch>
2021-09-24 16:39:49 +0200

    Script to start docker container and bind necessary volumes

---
+++ docker.sh
@@ -0,0 +1,6 @@
+docker run -d -it --name ML4Reco \
+-v /home/philipp/Data/Hackathon:/root/Data \
+-v "$(pwd)":/root/HGCalML \
+-v "/home/philipp/Data/pca-networks/":/root/pca-networks \
+-v /home/philipp/Data/Hackathon/ml4reco_sql.config:/root/private/ml4reco_sql.config \
+5a0776004b28
