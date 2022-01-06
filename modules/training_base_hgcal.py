ffe84bcc05ff01b59492838b186e164ce9dbb195
Jan Kieseler <jkiesele@cern.ch>
2022-01-06 11:10:46 +0100

    std redicrect

--- modules/training_base_hgcal.py
+++ modules/training_base_hgcal.py
@@ -1,8 +1,19 @@
 from DeepJetCore.training.training_base import training_base
 
 class HGCalTraining(training_base):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, 
+                 redirect_stdout=True,
+                 **kwargs):
+        '''
+        Adds file logging
+        '''
         super().__init__(*args, **kwargs)
+        
+        if redirect_stdout:
+            print('>>> redirecting the following stdout and stderr to logs in',self.outputDir)
+            import sys
+            sys.stdout = open(self.outputDir+'/stdout.txt', 'w')
+            sys.stderr = open(self.outputDir+'/stderr.txt', 'w')
 
     
     def trainModel(self,
