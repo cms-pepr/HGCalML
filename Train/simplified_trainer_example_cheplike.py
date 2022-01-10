420ba069ba618db2d6dd9794a440fe60849b918f
Jan Kieseler <jkiesele@cern.ch>
2022-01-10 15:48:22 +0100

    fixed plotting frequency

--- Train/simplified_trainer_example_cheplike.py
+++ Train/simplified_trainer_example_cheplike.py
@@ -300,7 +300,7 @@ import os
 samplepath=train.val_data.getSamplePath(train.val_data.samples[0])
 # publishpath = 'jkiesele@lxplus.cern.ch:/eos/home-j/jkiesele/www/files/HGCalML_trainings/'+os.path.basename(os.path.normpath(train.outputDir))
 
-plotfrequency=2
+plotfrequency=200
 
 publishpath = "jkiesele@lxplus.cern.ch:~/Cernbox/www/files/temp/Jan2022/"
 publishpath += [d  for d in train.outputDir.split('/') if len(d)][-1] 
