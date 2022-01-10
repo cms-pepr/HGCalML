more small fixes, synced inference distance and OC distance scaling finally

modules/lossLayers.py
@@ -559,6 +559,8 @@ class LLEdgeClassifier(LossLayerBase):
     
     def __init__(self, **kwargs):
         '''
+        Noise hits (truth index < 0) are never classified as belonging together
+        
         Inputs:
         - score
         - neighbour index
@@ -579,7 +581,8 @@ class LLEdgeClassifier(LossLayerBase):
         n_tidxs = SelectWithDefault(nidx, tidx, -1)# V x K x 1
         tf.assert_equal(tidx,n_tidxs[:,0]) #check that the nidxs have self-reference
         
-        n_tidxs = tf.where(n_tidxs<0,-20,n_tidxs)#set to -20 for noise
+        #int32 goes up to -2.something e-9
+        n_tidxs = tf.where(n_tidxs<0,-1000000000,n_tidxs)#set to -V for noise
         
         n_active = tf.where(nidx>=0, tf.ones_like(nidx,dtype='float32'), 0.)[:,1:] # V x K-1
         specweight = tf.clip_by_value(specweight,0.,1.)

modules/object_condensation.py
@@ -189,7 +189,7 @@ def oc_per_batch_element(
     distance_scale_kalpha_m_exp = tf.expand_dims(distance_scale_kalpha_m, axis=2) # K x 1 x 1
     
     distancesq_m = tf.reduce_sum( (tf.expand_dims(x_kalpha_m, axis=1) - x_m)**2, axis=-1, keepdims=True) #K x V-obj x 1
-    distancesq_m *= distance_scale_kalpha_m_exp**2
+    distancesq_m /= 2.*distance_scale_kalpha_m_exp**2
     
     absdist = tf.sqrt(distancesq_m + 1e-6)
     huberdistsq = huber(absdist, d=4) #acts at 4
@@ -226,7 +226,7 @@ def oc_per_batch_element(
     
     rep_distances = tf.reduce_sum(Mnot_distances**2, axis=-1, keepdims=True)  #K x V x 1
         
-    rep_distances *= distance_scale_kalpha_m_exp**2  #K x V x 1 , same scaling as attractive potential
+    rep_distances /= 2.*distance_scale_kalpha_m_exp**2  #K x V x 1 , same scaling as attractive potential
     
     V_rep =  tf.math.exp(-rep_distances) #1. / (V_rep + 0.1) #-2.*tf.math.log(1.-tf.math.exp(-V_rep/2.)+1e-5)
     

modules/plotting_callbacks.py
@@ -116,6 +116,8 @@ class plotClusteringDuringTraining(plotDuringTrainingBase):
             backgather = predicted[self.use_backgather_idx]
             truths = td.createTruthDict(feat)
             
+            
+            
             data = {}
             data.update(feats)
             data.update(truths)
@@ -125,6 +127,15 @@ class plotClusteringDuringTraining(plotDuringTrainingBase):
             
             data['recHitLogEnergy'] = np.log(data['recHitEnergy']+1)
             data['hitBackGatherIdx'] = backgather
+            
+            from globals import removed_noise_coordinate
+            removednoise = np.logical_and(data["recHitX"] == removed_noise_coordinate, 
+                                          data["recHitY"] == removed_noise_coordinate)
+            removednoise = np.logical_and(data["recHitZ"] == removed_noise_coordinate, 
+                                          removednoise)
+            #remove removed noise
+            for k in data.keys():
+                data[k] = data[k][not removednoise]
             
             df = pd.DataFrame (np.concatenate([data[k] for k in data],axis=1), columns = [k for k in data])
             
@@ -223,8 +234,17 @@ class plotEventDuringTraining(plotDuringTrainingBase):
             data['predCCoordsY'] = predCCoords[:,1:2]
             data['predCCoordsZ'] = predCCoords[:,2:3]
             
-            #for k in data:
-            #    print(k, data[k].shape)
+            from globals import removed_noise_coordinate
+            removednoise = np.logical_and(data["predCCoordsX"] == removed_noise_coordinate, 
+                                          data["predCCoordsY"] == removed_noise_coordinate)
+            removednoise = np.logical_and(data["predCCoordsZ"] == removed_noise_coordinate, 
+                                          removednoise)
+            removednoise = np.where(removednoise[:,0],False,True)
+            #remove removed noise
+            
+            for k in data.keys():
+                data[k] = data[k][removednoise]
+            
             
             df = pd.DataFrame (np.concatenate([data[k] for k in data],axis=1), columns = [k for k in data])
             