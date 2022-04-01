fb74ac00669612a765612595e36c014c3240a4d1
Saranya Ghosh <saranya.ghosh@cern.ch>
2022-04-01 15:59:17 +0000

    Adding interpretation

--- modules/datastructures/TrainData_ReductionNanoML.py
+++ modules/datastructures/TrainData_ReductionNanoML.py
@@ -26,17 +26,13 @@ class TrainData_ReductionNanoML(TrainData):
         self.cp_plus_pu_mode = False
         #reduction model used
         self.path_to_pretrained = os.getenv("HGCALML")+'/models/pre_selection_jan/KERAS_model.h5'
-        print("Preselection model used : ", self.path_to_pretrained)
-        #mo = load_model(self.path_to_pretrained)
-        #del mo
 
     def convertFromSourceFile(self, filename, weighterobjects, istraining, treename=""):
 
         #reduction model used
         #path_to_pretrained = os.getenv("HGCALML")+'/models/pre_selection_jan/KERAS_model.h5'
         model = load_model(self.path_to_pretrained)
-        #model = self.model
-        #print("Check Loaded preselection model : ", self.path_to_pretrained)
+        print("Loaded preselection model : ", self.path_to_pretrained)
 
 
         #outdict = model.output_shape
@@ -46,7 +42,6 @@ class TrainData_ReductionNanoML(TrainData):
                 list_outkeys.remove(l)
             elif l == "rs":
                 list_outkeys.remove(l)
-
         #print("Otput keys considered : ", list_outkeys)
 
         td = TrainData_NanoML()
@@ -61,16 +56,14 @@ class TrainData_ReductionNanoML(TrainData):
         gen.setBuffer(td)
 
         nevents = gen.getNBatches()
-        print("Nevents : ", nevents)
+        #print("Nevents : ", nevents)
 
         rs = []
         newout = {}
         rs.append(0)
 
-        #for i in range(5):
         for i in range(nevents):
             out = model(next(gen.feedNumpyData()))
-            #print("Check keys : ", out.keys())
             rs_tmp = out['rs'].numpy()
             rs.append(rs_tmp[1])
             if i == 0:
@@ -100,6 +93,47 @@ class TrainData_ReductionNanoML(TrainData):
                 outSA["t_idx"], outSA["t_energy"], outSA["t_pos"], outSA["t_time"],
                 outSA["t_pid"], outSA["t_spectator"], outSA["t_fully_contained"],
                 outSA["t_rec_energy"], outSA["t_is_unique"]],[], []
+
+
+    def interpretAllModelInputs(self, ilist, returndict=True):
+        #taken from TrainData_NanoML since it is similar, check for changes there
+        if not returndict:
+            raise ValueError('interpretAllModelInputs: Non-dict output is DEPRECATED. PLEASE REMOVE')
+        '''
+        input: the full list of keras inputs
+        returns: td
+         - rechit feature array
+         - t_idx
+         - t_energy
+         - t_pos
+         - t_time
+         - t_pid :             non hot-encoded pid
+         - t_spectator :       spectator score, higher: further from shower core
+         - t_fully_contained : fully contained in calorimeter, no 'scraping'
+         - t_rec_energy :      the truth-associated deposited
+                               (and rechit calibrated) energy, including fractional assignments)
+         - t_is_unique :       an index that is 1 for exactly one hit per truth shower
+         - row_splits
+
+        '''
+        out = {
+            'features':ilist[0],
+            'rechit_energy': ilist[0][:,0:1], #this is hacky. FIXME
+            't_idx':ilist[2],
+            't_energy':ilist[4],
+            't_pos':ilist[6],
+            't_time':ilist[8],
+            't_pid':ilist[10],
+            't_spectator':ilist[12],
+            't_fully_contained':ilist[14],
+            'row_splits':ilist[1]
+            }
+        #keep length check for compatibility
+        if len(ilist)>16:
+            out['t_rec_energy'] = ilist[16]
+        if len(ilist)>18:
+            out['t_is_unique'] = ilist[18]
+        return out
 
 
     def writeOutPrediction(self, predicted, features, truth, weights, outfilename, inputfile):
