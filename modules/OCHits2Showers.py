30c83b4360528be73892e4938e8d43416958a261
Shah Rukh Qasim <14beesqasim@seecs.edu.pk>
2022-06-09 14:34:19 +0200

    fix: fix non resolution of binned ops

--- modules/OCHits2Showers.py
+++ modules/OCHits2Showers.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
+from numba import njit
+from bin_by_coordinates_op import BinByCoordinates
 
 def reconstruct_showers_cond_op(cc, beta, beta_threshold=0.5, dist_threshold=0.5, limit=500, return_alpha_indices=False,
                                 pred_dist=None, max_hits_per_shower=-1,
@@ -54,18 +56,131 @@ def reconstruct_showers(cc, beta, beta_threshold=0.5, dist_threshold=0.5, pred_d
 
     return np.array(pred_sid)[:, np.newaxis], pred_shower_alpha_idx
 
-def reconstruct_showers_binned(cc, beta, beta_threshold=0.5, dist_threshold=0.5, pred_dist=None):
-    from assign_condensate_op import BinnedBuildAndAssignCondensates
+@njit
+def collect_func_native_gen(low_bin_indices,high_bin_indices, n_bins,flat_bin_finding_vector, assignment, coords, alpha_coords, alpha_radius,
+                        shower_idx, beta, row_splits, beta_filtered, beta_filtered_indices_full):
 
-    assignment, alpha_idx = BinnedBuildAndAssignCondensates(
-        cc,
-        beta,
-        row_splits=np.array([0, len(cc)], np.int32),
-        dist=pred_dist if pred_dist is not None else None,
-        min_beta=beta_threshold,
-        radius=dist_threshold)
+    range_bin_indices = high_bin_indices - low_bin_indices
+    ndim = len(high_bin_indices)
 
-    return assignment[..., np.newaxis], alpha_idx
+    bin_finding_div_vector = np.ones_like(low_bin_indices)
+    for i in range(ndim - 1):
+        bin_finding_div_vector[ndim - i - 2] = bin_finding_div_vector[ndim - i - 1] * range_bin_indices[ndim - i - 1]
+
+    total_iterations = np.prod(range_bin_indices)
+
+    for iteration in range(total_iterations):
+        bin_vector = low_bin_indices + (iteration // bin_finding_div_vector) % range_bin_indices
+        b_flat = np.sum(flat_bin_finding_vector * bin_vector)
+
+        # print(bin_vector)
+
+        if np.any(bin_vector >= n_bins):
+            continue
+
+        start_index = row_splits[b_flat]
+        end_index = row_splits[b_flat + 1]
+        for l in range(start_index, end_index):
+            if assignment[l] == -1:
+                if np.sum((coords[l] - alpha_coords) ** 2) < alpha_radius ** 2:
+                    assignment[l] = shower_idx
+                    beta[l] = 0
+                    if beta_filtered_indices_full[l] != -1:
+                        beta_filtered[beta_filtered_indices_full[l]] = 0
+
+@njit
+def collect_func_native(ix_l, ix_h, iy_l, iy_h, iz_l, iz_h, nbins, assignment, coords, alpha_coords, alpha_radius,
+                        shower_idx, beta, row_splits, beta_filtered, beta_filtered_indices_full):
+    x = 0
+    for i in range(ix_l, ix_h):
+        if i < 0 or i >= nbins:
+            continue
+        for j in range(iy_l, iy_h):
+            if j < 0 or j >= nbins:
+                continue
+            for k in range(iz_l, iz_h):
+                if k < 0 or k >= nbins:
+                    continue
+                b_flat = i * nbins ** 2 + j * nbins + k
+                start_index = row_splits[b_flat]
+                end_index = row_splits[b_flat + 1]
+                for l in range(start_index, end_index):
+                    if assignment[l] == -1:
+                        if np.sum((coords[l] - alpha_coords) ** 2) < alpha_radius ** 2:
+                            assignment[l] = shower_idx
+                            beta[l] = 0
+                            x += 1
+                            if beta_filtered_indices_full[l] != -1:
+                                beta_filtered[beta_filtered_indices_full[l]] = 0
+    return x
+
+
+
+def reconstruct_showers_binned(coords, beta, beta_threshold=0.3, dist_threshold=1.5, pred_dist=None):
+    beta = beta[:, 0]
+    coords = coords - np.min(coords, axis=0, keepdims=True)
+
+    if pred_dist is None:
+        pred_dist = 0 * beta + 1.0
+
+    pred_dist = pred_dist[:, 0] * dist_threshold
+
+    _, bins_flat, n_bins, bin_width, _ = BinByCoordinates(coords, [0, len(coords)], n_bins=30)
+
+    bins_flat = bins_flat.numpy()
+    n_bins = n_bins.numpy()
+    bin_width = float(bin_width[0])
+
+    bin_width_x = bin_width_y = bin_width_z = bin_width
+
+    sorting_indices = np.argsort(bins_flat)
+    beta = beta[sorting_indices]
+    coords = coords[sorting_indices]
+    bins_flat = bins_flat[sorting_indices]
+    pred_dist = pred_dist[sorting_indices]
+
+    row_splits = tf.ragged.segment_ids_to_row_splits(bins_flat,num_segments=np.prod(n_bins)).numpy()
+
+    flat_bin_finding_vector = np.concatenate((np.flip(np.cumprod(np.flip(n_bins)))[1:], [1]))
+
+    assignment = np.zeros_like(beta, dtype=np.int32) -1
+
+    shower_idx = 0
+    beta_filtered_indices = np.argwhere(beta > beta_threshold)[:, 0]
+    beta_filtered_indices_full = (assignment * 0 -1).astype(np.int32)
+    beta_filtered_indices_full[beta_filtered_indices] = np.arange(len(beta_filtered_indices))
+    beta_filtered = np.array(beta[beta_filtered_indices])
+
+    bin_width = np.array([bin_width_x, bin_width_y, bin_width_z])
+    pred_shower_alpha_idx = assignment * 0
+
+    while True:
+        alpha_idx = beta_filtered_indices[np.argmax(beta_filtered)]
+        max_beta = beta[alpha_idx]
+        # print("max beta", max_beta)
+        if max_beta < beta_threshold:
+            break
+        alpha_coords = coords[alpha_idx]
+        alpha_radius = pred_dist[alpha_idx]
+
+
+
+        low_bin_indices = np.floor((alpha_coords - alpha_radius)/bin_width).astype(np.int32)
+        high_bin_indices = np.ceil((alpha_coords + alpha_radius)/bin_width).astype(np.int32)
+        collect_func_native_gen(low_bin_indices, high_bin_indices, n_bins, flat_bin_finding_vector, assignment, coords, alpha_coords,
+                                alpha_radius, shower_idx, beta, row_splits, beta_filtered, beta_filtered_indices_full)
+
+
+        beta[alpha_idx] = 0
+        pred_shower_alpha_idx[shower_idx] = alpha_idx
+        shower_idx += 1
+
+    assignment_2 = assignment * 0
+    assignment_2[sorting_indices] = assignment
+    pred_shower_alpha_idx = pred_shower_alpha_idx[0:shower_idx]
+    pred_shower_alpha_idx = np.array([sorting_indices[i] for i in pred_shower_alpha_idx])
+
+    return assignment_2[..., np.newaxis], pred_shower_alpha_idx[0:shower_idx]
 
 
 
@@ -154,17 +269,6 @@ class OCHits2Showers():
                                                                             'pred_dist'] if self.with_local_distance_scaling else None)
         else:
             raise KeyError('%s reco method not recognized'%self.op)
-
-
-
-        if not os.path.exists('my_dat.bin'):
-            import gzip
-            with gzip.open('my_dat.bin', 'wb') as f:
-                import pickle
-                pickle.dump((features_dict, pred_dict, pred_sid, pred_shower_alpha_idx), f)
-            print("Dumped", os.getcwd())
-        else:
-            print("Not dumped", os.getcwd())
 
         processed_pred_dict = dict()
         processed_pred_dict['pred_sid'] = pred_sid
