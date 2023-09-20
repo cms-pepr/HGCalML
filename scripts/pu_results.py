f8844d095b2428cbc1ee112c0e3dee1b91305d63
Philipp Zehetner <philipp.zehetner@cern.ch>
2023-09-20 12:09:44 +0200

    update script

--- scripts/pu_results.py
+++ scripts/pu_results.py
@@ -1,3 +1,4 @@
+import pdb
 import os
 import sys
 import gzip
@@ -6,10 +7,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-BASEPATH = '/mnt/ceph/users/pzehetner/Paper/predictions'
 BASEPATH = '/home/philipp/FI2'
+BASEPATH = '/mnt/ceph/users/pzehetner/Paper/predictions'
 DATE = '0918'
-ANALYSISFILE = 'analysis_test_0.5_full.bin.gz'
+ANALYSISFILE = 'analysis_test_0.5_fullscan.bin.gz'
 OUTDIR = os.path.join(BASEPATH, 'pu_results.pkl')
 
 DIRS = [d for d in os.listdir(BASEPATH) if d.startswith(DATE)]
@@ -29,8 +30,10 @@ for i, d in enumerate(DIRS):
     n_events = np.max(showers['event_id']) + 1
     for event in range(n_events):
         shower = showers[(showers['event_id'] == event) & (showers['truthHitAssignementIdx'] == 0)]
+        if shower.shape[0] != 1:
+            pdb.set_trace()
 
-        if not shower.pred_enegy_hits.isna().any():
+        if not shower.pred_energy_hits.isna().any():
             e_true.append(shower.truthHitAssignedEnergies.iloc[0])
             e_pred.append(shower.pred_energy_hits.iloc[0])
             matched.append(event)
