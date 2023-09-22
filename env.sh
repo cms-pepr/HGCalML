a1e815de971d7c3cb6a6c603d6dc4ff0b06814dd
Jan Kieseler <jkiesele@cern.ch>
2023-09-22 06:55:55 -0400

    env for wandb

--- env.sh
+++ env.sh
@@ -10,3 +10,10 @@ export LC_ALL=C.UTF-8 	# necessary for wandb
 export LANG=C.UTF-8	# necessary for wandb
 #?export PYTHONPATH=$HGCALML/modules/datastructures:$PYTHONPATH
 #for ffmpeg
+
+# wandb api key
+# if ~/private/wandb_api.sh file exists then source from it
+if [[ -f ~/private/wandb_api.sh ]]; then
+   source ~/private/wandb_api.sh
+fi
+
