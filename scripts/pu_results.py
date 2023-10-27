c122772d03a76881a46e4f755d18ff3959c1292f
Philpp Zehetner <philipp.zehetner@cern.ch>
2023-10-27 08:35:00 -0400

    save more stuff

--- scripts/pu_results.py
+++ scripts/pu_results.py
@@ -4,6 +4,7 @@ import sys
 import gzip
 import pickle
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -11,15 +12,17 @@ BASEPATH = '/home/philipp/FI2'
 BASEPATH = '/mnt/ceph/users/pzehetner/Paper/predictions'
 DATE = '0918'
 ANALYSISFILE = 'analysis_test_0.5_fullscan.bin.gz'
-OUTDIR = os.path.join(BASEPATH, 'pu_results.pkl')
+ANALYSISFILE = 'analysis_hdbscan.bin.gz'
+ANALYSISFILE = 'classic_d025_i05.bin.gz'
+OUTFILE = os.path.join(BASEPATH, 'pu_results_200.pkl')
 
 DIRS = [d for d in os.listdir(BASEPATH) if d.startswith(DATE)]
 DIRS = [d for d in DIRS if os.path.exists(os.path.join(BASEPATH, d, ANALYSISFILE))]
 
 summary = {}
+dataframes = []
 
 for i, d in enumerate(DIRS):
-    if i > 0: break
 
     print(f"Scanning directory {d}")
     FILE = os.path.join(BASEPATH, d, ANALYSISFILE)
@@ -28,10 +31,13 @@ for i, d in enumerate(DIRS):
     with gzip.open(FILE, 'rb') as f:
         showers = pickle.load(f)['showers_dataframe']
     n_events = np.max(showers['event_id']) + 1
+    dataframe = pd.DataFrame()
     for event in range(n_events):
         shower = showers[(showers['event_id'] == event) & (showers['truthHitAssignementIdx'] == 0)]
+        dataframe = pd.concat((dataframe, shower))
         if shower.shape[0] != 1:
-            pdb.set_trace()
+            # pdb.set_trace()
+            print(d, "   ", event)
 
         if not shower.pred_energy_hits.isna().any():
             e_true.append(shower.truthHitAssignedEnergies.iloc[0])
@@ -44,5 +50,10 @@ for i, d in enumerate(DIRS):
         'e_true': np.array(e_true),
         'e_pred': np.array(e_pred),
         'matched': np.array(matched),
-        'unmatched': np.array(unmatched)
+        'unmatched': np.array(unmatched),
+        'showers': dataframe,
     }
+
+with open(OUTFILE, 'wb') as f:
+    pickle.dump(summary, f)
+
